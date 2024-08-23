24dffc0afa514c6056eb76eca1a038dd9e0b257e
majamarijan <majapianist@gmail.com>
2024-08-23 19:34:17 +0200

    data fetching type added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLoaderData } from "react-router-dom";
+import { Coctails } from "./routes/root";
+
+const ingredients:string[] = [];
+for(var i=1;i<=15;i++) {
+      ingredients.push(`strIngredient${i}`);
+  }
 
 export default function App() {
-  const ran = Math.floor(Math.random() * 1000);
+  const coctails = useLoaderData() as Coctails;  
+  
   return (
     <div className='wrapper'>
       <h1>Coctail App</h1>
-      <NavLink to={`/information/${ran}`}>Information</NavLink>
-      <NavLink to='/test'>Test</NavLink>
+      <div className="container">
+        {coctails && coctails.drinks.map(coctail => (
+          <NavLink key={coctail.idDrink} to={`/information/${coctail.strDrink}`}>
+            <div className="card">
+              <h2>{coctail.strDrink}</h2>
+              <div className="image">
+                <img src={coctail.strDrinkThumb} alt={coctail.strDrink} width='100' />
+              </div>
+              <div className="description">
+                <h3>Category: {coctail.strCategory}</h3>
+                <p>{coctail.strInstructions}</p>
+                <h3>Ingredients</h3>
+                <ul>
+                  {ingredients.filter(i => coctail[i]).map(i => <li key={i}>{coctail[i]}</li>)}
+                </ul>
+              </div>
+            </div>
+          </NavLink>
+        ))}
+      </div>
     </div>
   )
 }
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,9 +2,17 @@ import { Outlet } from "react-router-dom";
 import App from "../App";
 import {urls as data} from '../data';
 
-export async function rootLoader() {
+
+export type Coctails = {
+  drinks: {
+    [key: string]: string
+  }[]
+}
+
+export async function rootLoader(): Promise<Coctails> {
   const coctails = await fetch(data.coctails);
-  return coctails.json();
+  const json: Coctails = await coctails.json();
+  return json;
 }
 
 export default function Root() {
